3d73d273ebe96d841530a13b0fa1cb52387c0d97
Mr-Spicy123 <jspic1@ocdsb.ca>
2025-05-05 13:22:38 -0300

    update1

---
+++ index.js
@@ -0,0 +1,26 @@
+const playButton = document.getElementById("playBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const seekSlider = document.getElementById("seekSlider");
+
+// Create an audio object
+const audio = new Audio("ultimate-fart-sound-310131.mp3");
+
+audio.addEventListener("loadedmetadata", () => {
+    seekSlider.max = Math.floor(audio.duration);
+  });
+
+playButton.addEventListener("click", () => {
+  audio.play();
+});
+
+pauseBtn.addEventListener("click", () => {
+    audio.pause();
+  });
+
+  audio.addEventListener("timeupdate", () => {
+    seekSlider.value = Math.floor(audio.currentTime);
+  });
+  
+  seekSlider.addEventListener("input", () => {
+    audio.currentTime = seekSlider.value;
+  });
